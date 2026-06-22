5d720c7a5dc0721cfcf969dab9d8612610c3c6aa
jatinupadhyay112 <143349296+jatinupadhyay112@users.noreply.github.com>
2026-06-22 06:41:06 +0000

    some practice

---
+++ case_indentify.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char a;
+    cin>>a;
+    if(a<97){
+        cout<<"upper case";
+    }
+    else{
+        cout<<"lower case";
+    }
+
+}
