e0f15d62493e974dbc076ff0cc02b1806863e412
jatinupadhyay112 <143349296+jatinupadhyay112@users.noreply.github.com>
2026-06-23 20:42:26 +0530

    Update starsquare.cpp

--- starsquare.cpp
+++ starsquare.cpp
@@ -2,8 +2,10 @@
 using namespace std;
 int main()
 {
-	for(int i=0;i<5;i++){
-	    for(int j=0;j<5;j++){
+	int n;
+	cin>>n;
+	for(int i=1;i<=n;i++){
+	    for(int j=1;j<=n;j++){
 	        cout<<"*";
 	        }
 	    cout<<endl;
