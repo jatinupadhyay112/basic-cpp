964462dd1a4fc60f55f76446df47d228703c1e19
jatinupadhyay112 <143349296+jatinupadhyay112@users.noreply.github.com>
2026-06-25 14:40:44 +0530

    Create factorial_fn.cpp

---
+++ factorial_fn.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int fact (int n){
+    int fact=1;
+    for(int i =1;i<=n;i++){
+        fact=fact*i;
+    }
+    return fact;
+}
+int main(){
+        int n , fa;
+        cout<<"enter value";
+        cin>>n;
+        fa= fact(n);
+        cout<<fa;
+        
+    }
