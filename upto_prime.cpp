6b6603e2104828004f6be1e71e84b89316fd63d3
jatinupadhyay112 <143349296+jatinupadhyay112@users.noreply.github.com>
2026-06-26 08:47:53 +0000

    practice

--- upto_prime.cpp
+++ upto_prime.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+using namespace std;
+int upto_prime (int n){
+    int total=0;
+    for (int i=2;i<=n;i++){
+        int count=0;
+        for(int j=1;j<=(i/2);j++){
+            if(i%j==0){
+                count++;
+            }
+        }
+        if(count==1){
+            cout<<i<<",";
+            total++;
+        }
+    }
+    cout<<"\n Total prime nos. are "<<total;
+    return 0;
+}
+int main(){
+    int n;
+    cin>>n;
+    upto_prime(n);
+    return 0;
 
+}
