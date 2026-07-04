1e1e8cddee29851fb5ebb49de5b3906b9a11d80f
jatinupadhyay112 <143349296+jatinupadhyay112@users.noreply.github.com>
2026-07-04 05:43:28 +0000

    prac

--- reversearray.cpp
+++ reversearray.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int reverse(int arr[],int n){
+void reverse(int arr[],int n){
     int i=0,j=n-1,temp;
     while(i<j){
         temp=arr[i];
-        arr[i]=arr[j]
+        arr[i]=arr[j];
         arr[j]=temp;
         i++;
         j--;
     }    
+
 }
 
 int main(){
     int arr[]={3,4,2,0,6,4};
-    int n=6
-    indexofminmaxarr(arr ,n);
+    int n=6;
+    reverse(arr ,n);
+    for(int x :arr){
+        cout<<x<<endl;
+    }
+ 
     return 0;
 }
