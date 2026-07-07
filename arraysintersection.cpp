1b021fe9b8ca2320105b475959dbf49997f10153
jatinupadhyay112 <143349296+jatinupadhyay112@users.noreply.github.com>
2026-07-07 11:54:55 +0530

    prac

--- arraysintersection.cpp
+++ arraysintersection.cpp
@@ -2,9 +2,17 @@ void arrayintersection (int ar1[],int ar2[],int m,int n){
     for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
             if(ar1[i]==ar2[j]){
-                cout<<arr[i]<<',';
+                cout<<ar1[i]<<',';
                 break;
             }
         }
     }
 }
+
+int main(){
+    int ar1[]={2,4,3,9,1};
+    int ar2[]={1,3,5,6,8};
+    int m=size(ar1);
+    int n=size(ar2);
+    arrayintersection(ar1,ar2,m,n);
+    }
